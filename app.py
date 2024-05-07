2825a817586300930fb63d90c24d5ef89b5ee7b5
Thiago Lopes <thiagoelopes199@gmail.com>
2024-05-07 12:16:27 -0300

    Segundo Comitt

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 n1 = float(input("primeiro numero: "))
 n2 = float(input("segundo numero: "))
 funcao = str(input(
-    "selecione uma funcao: soma(s), subtracao(m) multiplicacao(v) divisao(d) -> "))
+    "selecione uma funcao: adicão(a), subtracao(s) multiplicacao(m) divisao(d) -> "))
 
 print("resultado:", end="")
-if funcao == "s":
+if funcao == "a":
     print(n1 + n2)
-elif funcao == "m":
+elif funcao == "s":
     print(n1 - n2)
-elif funcao == "v":
+elif funcao == "m":
     print(n1 * n2)
 elif funcao == "d":
     print(n1 / n2)
